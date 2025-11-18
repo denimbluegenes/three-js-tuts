9f91f29b90f4805617b1f38f1534f45cd4abaac6
denimbluegenes <erilynmle@gmail.com>
2025-11-18 02:17:08 -0800

    cubes

--- examples/three-00-starter/js/main.js
+++ examples/three-00-starter/js/main.js
@@ -11,12 +11,58 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 // ~~~~~~~~~~~~~~~~Create scene here~~~~~~~~~~~~~~~~
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+let scene, camera, renderer, cube;
+
+function init() {
+  scene = new THREE.Scene();
+
+camera = new THREE.PerspectiveCamera(
+  75, 
+  window.innerWidth / window.innerHeight,
+  0.1, 
+  1000
+);
+
+renderer = new THREE.WebGLRenderer({ antialias: true });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.TorusGeometry(2, 0.6, 16, 100);
+//const material = new THREE.MeshBasicMaterial({ map: texture });;
+
+const texture = new THREE.TextureLoader().load('textures/ice.jpg')
+const material = new THREE.MeshBasicMaterial( { map: texture } );
+
+cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+}
+
+
+function animate() {
+  requestAnimationFrame(animate);   // ⬅ keep calling animate
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
 
 
 
